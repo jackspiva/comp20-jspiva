3ec043048374fc5caaee1a453bd2ef500c466e83
John Spiva <johnspiva@gmail.com>
2015-10-12 21:15:46 -0400

    setup barebones init function in game.js

--- duckhunt/game.js
+++ duckhunt/game.js
@@ -1,1 +1,7 @@
-// Replace this with your code...
+
+function init(){
+    var canvas = document.getElementById('game_canvas');        //  Learned from Mozilla canvas tutorial
+    if (canvas.getContext){                                     //  at https://developer.mozilla.org/en-US/docs/Web/API/Canvas_API/Tutorial
+              var ctx = canvas.getContext('2d');
+    }
+}
