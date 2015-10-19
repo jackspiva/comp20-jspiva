5ba171c772600f6d7052441172660d88be083931
John Spiva <johnspiva@gmail.com>
2015-10-18 21:21:49 -0400

    added code to open json file

--- messages/lab.js
+++ messages/lab.js
@@ -1,1 +1,18 @@
-// Your JavaScript goes here...
+function parse() {
+    var xmlhttp = new XMLHttpRequest();
+    var url = "data.json";
+
+    xmlhttp.onreadystatechange = function() {
+        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+            var parsed_data = JSON.parse(xmlhttp.responseText);
+            output(parsed_data);
+        }
+    }
+
+    xmlhttp.open("GET", url, true);
+    xmlhttp.send();
+}
+
+function output(parsed_data) {
+    console.log(parsed_data.length)
+}
