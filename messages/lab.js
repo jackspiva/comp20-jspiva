7a27947a75df9175cae05c5432b51195044fc3d0
John Spiva <johnspiva@gmail.com>
2015-10-18 22:02:03 -0400

    added some comments to javascript

--- messages/lab.js
+++ messages/lab.js
@@ -1,11 +1,11 @@
-function parse() {
-    var xmlhttp = new XMLHttpRequest();
-    var url = "data.json";
+function parse() {                                                  // XMLHttpRequest code based
+    var xmlhttp = new XMLHttpRequest();                             // off the example code at
+    var url = "data.json";                                          // http://www.w3schools.com/json/json_http.asp
 
     xmlhttp.onreadystatechange = function() {
         if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-            var parsed_data = JSON.parse(xmlhttp.responseText);
-            output(parsed_data);
+            var parsed_data = JSON.parse(xmlhttp.responseText);     // parse data
+            output(parsed_data);                                    // send parsed data do output function
         }
     }
 
@@ -16,11 +16,11 @@ function parse() {
 function output(parsed_data) {
     var messages = "";
     var i;
-    for(i = 0; i < parsed_data.length; i++) {
+    for(i = 0; i < parsed_data.length; i++) {                           // create html to add into index.html
         messages += '<p class="message" id="parsed.data[i].id">' +
                     '<span class="username">'   + parsed_data[i].username   + '</span>'     + ": " +
                     '<span class="content">'    + parsed_data[i].content    + '</span></p>'
 
     }
-    document.getElementById("messages").innerHTML = messages;
+    document.getElementById("messages").innerHTML = messages;           // add html into index.html
 }
